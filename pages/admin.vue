556996671b99cbd0e7af30a3b8e49c212aa362af
Olivia Igbinosa <oi506332@gmail.com>
2026-08-13 12:22:08 +0100

    fix

--- pages/admin.vue
+++ pages/admin.vue
@@ -26,7 +26,7 @@
             <path d="M23 21v-2a4 4 0 0 0-3-3.87" />
             <path d="M16 3.13a4 4 0 0 1 0 7.75" />
           </svg>
-          {{ userRole === 'super admin' ? 'SUPER ADMIN VIEW' : 'ADMIN VIEW' }}
+          {{ isSuperAdmin ? 'SUPER ADMIN VIEW' : 'ADMIN VIEW' }}
         </span>
       </div>
     </div>
@@ -507,7 +507,7 @@
             />
             <span v-if="onboardErrors.department" class="err-msg">{{ onboardErrors.department }}</span>
           </div>
-          <div v-if="userRole === 'super admin'" ref="onboardRoleDropdownRef" class="field onboarding-field custom-select">
+          <div v-if="isSuperAdmin" ref="onboardRoleDropdownRef" class="field onboarding-field custom-select">
             <label class="mono-label">Role </label>
             <button
               type="button"
